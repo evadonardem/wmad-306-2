da3a402e96694460d25ccbfbef822968392ad2b9
Ho, Mark Anthony L <2024-1-0736.ho@kcp.edu.ph>
2026-04-24 18:57:35 +0800

    06_Adopt_A_Dog_App_Ho

--- adopt_a_dog/android/app/src/main/kotlin/com/example/adopt_a_dog/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.adopt_a_dog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- adopt_a_dog/lib/models/breed.dart
+++
@@ -1,8 +0,0 @@
-class Breed {
-  final String name;
-  final List<String> subBreeds;
-
-  const Breed({required this.name, required this.subBreeds});
-
-  String displayName({String? sub}) => sub != null ? "$sub $name" : name;
-}
