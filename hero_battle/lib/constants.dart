1033c12e1cf128784adefff8dea23317d5448163
Dave Medrano <evadonardem@gmail.com>
2026-05-02 08:06:03 +0800

    Removed unnecessary directories and files.

--- hero_battle/lib/constants.dart
+++
@@ -1,3 +0,0 @@
-const String kApiToken = '69e1ad2b795146afb3b7b227ba460dbe';
-const String kApiBaseUrl = 'https://superheroapi.com/api/';
-const int kMaxDeckSize = 5;
--- hero_battle/lib/router/route_names.dart
+++
@@ -1,9 +0,0 @@
-class RouteNames {
-  static const String splash = '/';
-  static const String home = '/home';
-  static const String heroDetail = '/hero-detail';
-  static const String deckBuilder = '/deck-builder';
-  static const String battle = '/battle';
-  static const String history = '/history';
-  static const String profile = '/profile';
-}
